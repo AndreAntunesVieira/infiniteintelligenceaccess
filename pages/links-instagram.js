6454b48f25f17c9f12418171e90467b71c08f9e5
Andre Antunes Vieira <andreantunesv@gmail.com>
2023-04-18 13:47:43 -0300

    realigin

--- pages/links-instagram.js
+++ pages/links-instagram.js
@@ -24,20 +24,20 @@ export default function LinksInstagram() {
         rel="stylesheet"/>
       </Head>
       <main id="links-instagram">
-        <div id="languages">
-          <a href="/pt/links-instagram">
-            <Image width={30} height={20} src="/bandeira-brazil.png" className={classNames("language-selector", {selected: locale === 'pt'})}/>
-          </a>
-          <a href="/en/links-instagram">
-            <Image width={30} height={20} src="/bandeira-usa.png" className={classNames("language-selector", {selected: locale === 'en'})}/>
-          </a>
-          <a href="/nl/links-instagram">
-            <Image width={30} height={20} src="/bandeira-nl.png" className={classNames("language-selector", {selected: locale === 'nl'})}/>
-          </a>
-        </div>
-
+      
         <div id="brand">
           <img src="/logo-infiniteintelligenceaccess.png"/>
+          <div id="languages">
+          <a href="/pt/links-instagram">
+            <Image width={40} height={30} src="/bandeira-brazil.png" className={classNames("language-selector", {selected: locale === 'pt'})}/>
+          </a>
+          <a href="/en/links-instagram">
+            <Image width={40} height={30} src="/bandeira-usa.png" className={classNames("language-selector", {selected: locale === 'en'})}/>
+          </a>
+          <a href="/nl/links-instagram">
+            <Image width={40} height={30} src="/bandeira-nl.png" className={classNames("language-selector", {selected: locale === 'nl'})}/>
+          </a>
+        </div>
         </div>
         <div>
           <h1>
@@ -54,36 +54,36 @@ export default function LinksInstagram() {
             </a>
           </li>
           <li>
-            <a href="#" target="_blank">
+            <a href="https://forms.gle/vgkti5CVWVmBq45P9" target="_blank">
               <span>{getString('links-instagram.anonymous-agreed-group')}</span>
             </a>
           </li>
           <li>
-            <a href="#" target="_blank">
+            <a href={getString('links-instagram.join-group-webnar-form')} target="_blank">
               <span>{getString('links-instagram.join-group-webnar')}</span>
             </a>
           </li>
-          <li>
+          {/* <li>
             <a href="#" target="_blank">
               <span>{getString('links-instagram.my-website')}</span>
               <Image src="/globe.png" alt="Globe" width={30} height={30}/>
             </a>
-          </li>
+          </li> */}
         </ul>
         <ul id="social-media">
           <li>
             <a href="https://www.instagram.com/infiniteintelligenceaccess/" target="_blank">
-                <Image src="/cartao_files/3a8d7aacda991695cb3cbe37cfe32d9d.svg" alt="Globe" width={30} height={30}/>
+                <Image src="/cartao_files/3a8d7aacda991695cb3cbe37cfe32d9d.svg" alt="Globe" width={50} height={50}/>
               </a>
           </li>
           <li>
             <a href="https://www.youtube.com/@infiniteintelligenceaccess5673/videos" target="_blank">
-                <Image src="/cartao_files/08cd78ea2560593f6c4cc27a1b52fd9c.svg" alt="Globe" width={30} height={30}/>
+                <Image src="/cartao_files/08cd78ea2560593f6c4cc27a1b52fd9c.svg" alt="Globe" width={50} height={50}/>
               </a>
           </li>
           <li>
             <a href="https://www.tiktok.com/@karinerodriguesholistic" target="_blank">
-                <Image src="/cartao_files/ae5d41507e04beb3f3466f23c04a36c3.svg" alt="Globe" width={30} height={30}/>
+                <Image src="/cartao_files/ae5d41507e04beb3f3466f23c04a36c3.svg" alt="Globe" width={50} height={50}/>
               </a>
           </li>
             
